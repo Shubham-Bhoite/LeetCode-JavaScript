1cc1488e984f00d1c6990880f15f2c43fe98da4b
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-12-17 10:05:48 +0530

    solve Return Length of Arguments Passed

---
+++ Solutions/Problem-09.js
@@ -0,0 +1,9 @@
+/* 9.Return Length of Arguments Passed :
+ Write a function argumentsLength that returns the count of arguments passed to it.
+  */
+
+
+ var argumentsLength = function(...args) {
+    return args.length
+ 
+};
