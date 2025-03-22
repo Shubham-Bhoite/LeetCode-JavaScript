c0a6c20df6b568f30c2c5f3b4f2c5fb8b3d39b71
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-03-22 10:48:13 +0530

    update

--- Solutions/Problem-22.js
+++ Solutions/Problem-22.js
@@ -1,4 +1,4 @@
-/* 22.Promise Time Limit
+/* 22.Promise Time Limit :
 
 Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
 
--- Solutions/Problem-23.js
+++ Solutions/Problem-23.js
@@ -1,3 +1,5 @@
-/* 23.Debounce  
+/* 23.Debounce  :
 
-*/
+Given a function fn and a time in milliseconds t, return a debounced version of that function.
+
+A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. The debounced function should also receive the passed parameters.*/
