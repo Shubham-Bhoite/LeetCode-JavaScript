2252ccbf87bbe25272f794fb9235b31909b65766
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-03-30 09:54:21 +0530

    Add solution

--- Solutions/Problem-25.js
+++ Solutions/Problem-25.js
@@ -7,3 +7,14 @@ A grouped array is an object where each key is the output of fn(arr[i]) and each
 The provided callback fn will accept an item in the array and return a string key.
 The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
 */
+
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc;
+    }, {});
+};
