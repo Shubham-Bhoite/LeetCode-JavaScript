f63ba7c6166cc524c312fd3e76b6afd717bbd7e7
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-04-12 09:48:33 +0530

    add problem-statement

--- Solutions/Problem-28.js
+++ Solutions/Problem-28.js
@@ -1,1 +1,4 @@
-/* 28. Event Emitter : */
+/* 28. Event Emitter :
+
+Design an EventEmitter class. This interface is similar (but with some differences) to the one found in Node.js or the Event Target interface of the DOM. The EventEmitter should allow for subscribing to events and emitting them.
+*/
