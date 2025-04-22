2aca6306068e8bdd695005d645e5aa81520f2dc6
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-04-22 11:22:37 +0530

    add problem

--- Solutions/Problem-30.js
+++ Solutions/Problem-30.js
@@ -1,1 +1,12 @@
-/* 30. Array Wrapper : */
+/* 30.Cache With Time Limit : 
+
+Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+The class has three public methods:
+
+1) set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
+
+2) get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+
+3) count(): returns the count of un-expired keys.
+
+*/
