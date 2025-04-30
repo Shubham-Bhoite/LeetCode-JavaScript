94f46853f306fe2cb100bb23ac599ba19dca24cf
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-04-30 10:53:39 +0530

    solved array of number

---
+++ Practice Problems/6.Array.js
@@ -0,0 +1,16 @@
+// 6. Write a JavaScript function that uses an array of numbers and only gives a new array with even numbers :
+
+
+function evenNumbers(numbers) {
+    const evenArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenArray.push(numbers[i]);
+        }
+    }
+    return evenArray;
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const evenNumbersArray = evenNumbers(numbers);
+console.log("Even numbers:", evenNumbersArray);
+
