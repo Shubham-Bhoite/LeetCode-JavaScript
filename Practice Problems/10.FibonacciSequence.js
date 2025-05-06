7cfe85b46ac896ce9da3ee6a98cbec5f3a022ca3
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-05-06 09:34:28 +0530

    solved Fibonacci sequence of number

---
+++ Practice Problems/10.FibonacciSequence.js
@@ -0,0 +1,20 @@
+// 10. Write a JavaScript function that returns the Fibonacci sequence to a specified number :
+
+
+function fibonacci(n) {
+    if (n <= 0) {
+        return [];
+    }
+    if (n === 1) {
+        return [0]; // Return [0] if n is 1
+    }
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+}
+const terms = 10;
+const fibonacciSequence = fibonacci(terms);
+console.log(fibonacciSequence);
+
