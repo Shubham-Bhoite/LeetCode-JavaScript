5e44b2894b9cff0c5af8471ee417192a28880bc4
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-05-24 11:10:43 +0530

    add problem solution

--- Practice Problems/26.Array6.js
+++ Practice Problems/26.Array6.js
@@ -1,3 +1,16 @@
 // 26. Find the maximum difference between two numbers in an array in JavaScript :
 
 
+function maxDifference(arr) {
+    let min = arr[0]
+    let maxDiff = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        const diff = arr[i] - min;
+        maxDiff = Math.max(maxDiff, diff);
+        min = Math.min(min, arr[i]);
+    }
+    return maxDiff;
+}
+
+console.log(maxDifference([1, 2, 90, 10, 110]));
