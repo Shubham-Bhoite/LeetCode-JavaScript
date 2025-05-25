905c39db1505c1d0daa1c3f742e6c45f67fcb46e
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-05-25 10:09:33 +0530

    solved vowels in string

---
+++ Practice Problems/27.Vowels.js
@@ -0,0 +1,17 @@
+// 27. Count Vowels in a String in JavaScript :
+
+
+function countVowels(str) {
+    let count = 0;
+    // Include both lowercase and uppercase vowels
+    const vowels = 'aeiouAEIOU'; 
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("hello bro"));
