31eaa1efb747228b03a1a9df07ce904edbd3682e
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2025-05-27 11:40:03 +0530

    solved unique character from string

--- Practice Problems/28.String3.js
+++ Practice Problems/28.String3.js
@@ -1,1 +1,14 @@
 // 28. Get Unique Characters from a String in JavaScript :
+
+
+function uniqueCharacters(str) {
+    const uniqueChars = [];
+    for (let i = 0; i < str.length; i++) {
+        if (!uniqueChars.includes(str[i])) {
+            uniqueChars.push(str[i]);
+        }
+    }
+    return uniqueChars.join('');
+}
+
+console.log(uniqueCharacters("geeksforgeeks")); 
